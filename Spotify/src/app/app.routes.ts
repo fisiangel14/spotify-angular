cf01396d8eadb00b60cab7bce3b0adebd8dcf70f
Angel Alegre <fisiangel.14@gmail.com>
2025-09-22 23:51:32 -0500

    add compoonets but there are errors

--- Spotify/src/app/app.routes.ts
+++ Spotify/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'auth', pathMatch: 'full' },
+
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./pages/auth/login-page').then(m => m.LoginPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./pages/home/home-page').then(m => m.HomePage)
+  },
+  {
+    path: 'history',
+    loadComponent: () =>
+      import('./pages/history/history-page').then(m => m.HistoryPage)
+  },
+  {
+    path: 'tracks',
+    loadComponent: () =>
+      import('./pages/tracks/tracks-page').then(m => m.TracksPage)
+  },
+
+  { path: '**', redirectTo: 'auth' }
+];
---
+++ Spotify/src/app/pages/tracks/tracks-page/tracks-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracks-page',
+  imports: [],
+  templateUrl: './tracks-page.html',
+  styleUrl: './tracks-page.css'
+})
+export class TracksPage {
+
+}
