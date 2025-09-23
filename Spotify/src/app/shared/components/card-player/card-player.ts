b6bac207e971b7dcadf59dc5f25448dcfb7a5aa5
Angel Alegre <fisiangel.14@gmail.com>
2025-09-23 17:24:27 -0500

    bug css

--- Spotify/src/app/shared/components/card-player/card-player.ts
+++ Spotify/src/app/shared/components/card-player/card-player.ts
@@ -1,14 +1,19 @@
 import { Component,Input } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-player',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-player.html',
   styleUrl: './card-player.css'
 })
 export class CardPlayer {
   @Input() data: any;
-  @Input() track!: TrackModel; // 👈 este es el que recibes desde SectionGeneric
+  @Input() track: TrackModel = {_id: 0,
+    name: '',
+    album: '',
+    url: '',
+    cover: ''}; // 👈 este es el que recibes desde SectionGeneric
   @Input() mode: 'small' | 'big' = 'small';
 }
--- Spotify/src/app/shared/components/section-generic/section-generic.ts
+++ Spotify/src/app/shared/components/section-generic/section-generic.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgClass } from '@angular/common';
 import { TrackModel } from '@core/models/tracks.model'; 
+import { CardPlayer } from '../card-player/card-player';
 
 @Component({
   selector: 'app-section-generic',
-  imports: [CommonModule, NgClass],
+  imports: [CommonModule, NgClass, CardPlayer],
   templateUrl: './section-generic.html',
   styleUrl: './section-generic.css'
 })
