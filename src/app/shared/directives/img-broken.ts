ccc2cfde05f2696453921cb459ca4e40fb0d21eb
Angel Alegre <fisiangel.14@gmail.com>
2025-10-05 12:37:50 -0500

    add directivas in case iamgen dont exits this laod image

--- src/app/shared/directives/img-broken.ts
+++ src/app/shared/directives/img-broken.ts
@@ -1,18 +1,21 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: 'img[appImgBroken]'
 })
 export class ImgBroken {
 
-  @HostListener('error') hableError():void{
+  @Input() customImg: string = '';
+
+  @HostListener('error') handleError():void{
     const elNative = this.elHost.nativeElement;
     console.log('Error al cargar la imagen-->', this.elHost);
-    elNative.src ='../../../../assets/images/imgBroken.png'
+    // elNative.src = 'assets/images/imgBroken.png';
+    elNative.src = this.customImg;
   }
 
   constructor(private elHost: ElementRef) {
     console.log(this.elHost);
    }
-
+   
 }
